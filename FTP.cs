bc1ac5699d67aba7b9d12d3afe0454085f9c914d
ladn00 <Andrej.zuev.2918@gmail.com>
2024-11-18 01:32:22 +0500

    Добавьте файлы проекта.

---
+++ FTP.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+namespace FTP_client
+{
+    using System.Net;
+    using System.IO;
+    using System.Collections.Generic;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
+
+    public class FTP
+    {
+        private string _host;
+        private NetworkCredential _credentials;
+        private WebClient _wc;
+
+        public FTP(string host, string username, string password)
+        {
+            if (!host.StartsWith("http://") && !host.StartsWith("https://") && !host.StartsWith("ftp://"))
+            {
+                host = "https://" + host;
+            }
+
+            _host = host;
+            _credentials = new(username, password);
+
+            _wc = new()
+            {
+                BaseAddress = _host,
+                Credentials = _credentials
+            };
+        }
+
+        public bool UploadFile(string localFile, string remoteFile)
+        {
+            try
+            {
+                _wc.UploadFile(remoteFile, localFile);
+                return true;
+            }
+            catch(Exception ex) {
+
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        private WebRequest CreateRequest(string path, string method)
+        {
+            var req = WebRequest.Create(_host + path);
+            req.Method = method;
+            req.Credentials = _credentials;
+            return req;
+        }
+
+        public bool DownloadFile(string remoteFile, string localFile)
+        {
+            try
+            {
+                _wc.DownloadFile(remoteFile, localFile);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool CreateDirectory(string remoteDir)
+        {
+            try
+            {
+                var req = CreateRequest(remoteDir, WebRequestMethods.Ftp.MakeDirectory);
+                var resp = req.GetResponse();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public List<string> GetFiles(string remoteDir)
+        {
+            try
+            {
+                var req = CreateRequest(remoteDir, WebRequestMethods.Ftp.ListDirectory);
+                var resp = req.GetResponse();
+                var sr = new StreamReader(resp.GetResponseStream());
+                var l = new List<string>();
+
+                string s = null;
+                while (true)
+                {
+                    if (string.IsNullOrEmpty(s = sr.ReadLine()))
+                    {
+                        break;
+                    }
+                    l.Add(s);
+                }
+
+                sr.Close();
+
+                return l;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using FTP_client;
+
+FTP ftp = new("195.161.68.19", "j02458176", "178982az");
+Console.WriteLine(ftp.DownloadFile("http://183272a7c67f.hosting.myjino.ru/05%20-%20No%20One%20Left.mp3", @"C:\\Users\\USER\\Desktop\\file.mp3"));
+Console.WriteLine(ftp.UploadFile(@"C:\\Users\\USER\\Desktop\\file2.mp3", @"https://183272a7c67f.hosting.myjino.ru/res/file.mp3"));
