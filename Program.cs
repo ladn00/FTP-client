f29f784de514d0f20f05ab702f5f7f32abb13241
Андрей Зуев <andrej.zuev.2918@mail.ru>
2024-11-18 09:48:36 +0500

    upload files works finally

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using FTP_client;
 
 FTP ftp = new("195.161.68.19", "j02458176", "178982az");
-Console.WriteLine(ftp.DownloadFile("http://183272a7c67f.hosting.myjino.ru/05%20-%20No%20One%20Left.mp3", @"C:\\Users\\USER\\Desktop\\file.mp3"));
-Console.WriteLine(ftp.UploadFile(@"C:\\Users\\USER\\Desktop\\file2.mp3", @"https://183272a7c67f.hosting.myjino.ru/res/file.mp3"));
+// Console.WriteLine(ftp.DownloadFile("http://183272a7c67f.hosting.myjino.ru/05%20-%20No%20One%20Left.mp3", @"C:\Users\andre\Desktop\file.mp3"));
+Console.WriteLine(ftp.UploadFile(@"file.txt", @"ftp://183272a7c67f.hosting.myjino.ru/domains/183272a7c67f.hosting.myjino.ru/file.txt"));
